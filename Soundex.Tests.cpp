54c84d4dc7c4f6fde8c43e0fae0746150a9d719d
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-03 10:03:44 +0530

    Added test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,61 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+    char soundex[5];
+    generateSoundex("AX", soundex);
+    ASSERT_STREQ(soundex, "A200");
+}
+
+TEST(SoundexTestsuite, IgnoresNonAlphabeticCharacters) {
+    char soundex[5];
+    generateSoundex("A1X", soundex);
+    ASSERT_STREQ(soundex, "A200");
+}
+
+TEST(SoundexTestsuite, HandlesEmptyString) {
+    char soundex[5];
+    generateSoundex("", soundex);
+    ASSERT_STREQ(soundex, "0000");
+}
+
+TEST(SoundexTestsuite, HandlesSingleCharacterString) {
+    char soundex[5];
+    generateSoundex("A", soundex);
+    ASSERT_STREQ(soundex, "A000");
+}
+
+TEST(SoundexTestsuite, HandlesAlreadyCodedString) {
+    char soundex[5];
+    generateSoundex("BFPV", soundex);
+    ASSERT_STREQ(soundex, "B100");
+}
+
+TEST(SoundexTestsuite, HandlesDifferentSoundexCodesForSameChar) {
+    char soundex[5];
+    generateSoundex("CGJKQSXZ", soundex);
+    ASSERT_STREQ(soundex, "C222");
+}
+
+TEST(SoundexTestsuite, HandlesVowelCharacters) {
+    char soundex[5];
+    generateSoundex("AEIOUHWY", soundex);
+    ASSERT_STREQ(soundex, "A000");
+}
+
+TEST(SoundexTestsuite, HandlesMultipleSameSoundexCodeCharacters) {
+    char soundex[5];
+    generateSoundex("TTTT", soundex);
+    ASSERT_STREQ(soundex, "T300");
+}
+
+TEST(SoundexTestsuite, HandlesDifferentSoundexCodes) {
+    char soundex[5];
+    generateSoundex("ABCDEFG", soundex);
+    ASSERT_STREQ(soundex, "A123");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
