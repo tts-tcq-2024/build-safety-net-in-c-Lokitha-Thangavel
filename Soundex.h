45724ee05de5234ce0123d184512a74160b3e2f1
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-07 20:30:03 +0530

    Separated functions

--- Soundex.h
+++ Soundex.h
@@ -4,39 +4,50 @@
 #include <ctype.h>
 #include <string.h>
 
-char getSoundexCode(char c) {
-    static const char codeMap[26] = {
+char getSoundexCode(char c) 
+{
+    static const char codeMap[26] = 
+    {
         '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '0', '0', '2', '0', '2'
     };
-    if (isalpha(c)) {
+    if (isalpha(c)) 
+    {
         return codeMap[toupper(c) - 'A'];
     }
     return '0';
 }
 
-void generateSoundex(const char *name, char *soundex) {
-    if (name[0] == '\0') {
+void padWithZeros(char *soundex, int startIndex) 
+{
+    while (startIndex < 4) 
+    {
+        soundex[startIndex++] = '0';
+    }
+    soundex[4] = '\0';
+}
+
+void generateSoundex(const char *name, char *soundex)
+{
+    if (name[0] == '\0')
+    {
         strcpy(soundex, "0000");
         return;
     }
-
+    
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
     char prevCode = getSoundexCode(soundex[0]);
 
-    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
+    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) 
+    {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
+        if (code != '0' && code != prevCode) 
+        {
             soundex[sIndex++] = code;
             prevCode = code;
         }
     }
-
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
-
-    soundex[4] = '\0';
+    padWithZeros(soundex, sIndex);    
 }
 
 #endif // SOUNDEX_H
